77de383e0281fef45cab7ed39e06284b9bde17ab
Caio Balthazar <balthacv@rose-hulman.edu>
2012-05-02 16:55:19 -0400

    Code to pass the last features tests

--- ChatBot-balthacv/chatbot.rb
+++ ChatBot-balthacv/chatbot.rb
@@ -2,3 +2,14 @@ STDOUT.sync = true
 puts 'What is your name?'
 name = gets.chomp
 puts "Hello, #{name}!"
+puts "Choose the game:"
+list = ["1- WoW","2- DotA","3- Global Thermonuclear War","4- AngryBirds","5- Chess"]
+list.each do |element|
+	puts element
+end
+chosen = gets.chomp
+if chosen == "3"
+	puts "BOOM!"
+else
+	puts "Not playing that game. Bye."
+end
